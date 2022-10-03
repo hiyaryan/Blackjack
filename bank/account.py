11c384b67e0f9e11b414c9bdc81a02935cec49fa
Replit user <>
2022-10-03 21:22:09 +0000

    Add ability to purchase tokens.

--- bank/account.py
+++ bank/account.py
@@ -1,5 +1,6 @@
 # Account
 from simple_term_menu import TerminalMenu
+from bank.token import Token
 
 class Account:
     def __init__(self, name):
@@ -11,7 +12,7 @@ class Account:
         
         self.name = name
         self.balance = 0
-        self.tokens = {}
+        self.tokens = {'1': [], '5': [], '10': [], '20': [], '100': []}
     
     def deposit(self, funds):
         '''
@@ -26,8 +27,54 @@ class Account:
         Options: E = Exchange, W = Win
         '''
         if opt == 'E':
-            print(f"{self.name} is buying tokens...")
-            
+
+            # Input dollar amount.
+            # value = None
+            # while not value:
+            #     try:
+            #         value = int(input("\nEnter a full dollar amount.\n\tValue: $"))
+
+            #         if value <= 0:
+            #             print("\nInvalid input: Please enter a value greater than 0.\n")
+            #             value = None
+                
+            #     except ValueError:
+            #         print("\nInvalid input: Please enter a number only.\n")
+
+            # Input number of tokens
+            token_options = ["[1] 1", "[2] 5", "[3] 10", "[4] 20", "[5] 100", "[d] Done"]
+            token_options_dict = {0: '1', 1: '5', 2: '10', 3: '20', 4: '100', 5: "Done"}
+
+            token_menu = TerminalMenu(token_options, title="\n- Tokens -\n")
+            token_selection = token_options_dict[token_menu.show()]
+
+            total = 0
+            value = 0
+            while token_selection != 'Done':
+                amount = None
+                while not amount:
+                    try:
+                        amount = int(input(f"\nEnter the number of ¢{token_selection} tokens desired.\n\tAmount: "))
+                    
+                        if amount < 0:
+                            print("\nInvalid input: Please enter a value greater than or equal to 0.\n")
+                            amount = None
+                            
+                        else:
+                            if amount > 0:
+                                for i in range(amount):
+                                    self.tokens[token_selection].append(Token(token_selection, int(token_selection)))
+                                    value += int(token_selection)
+
+                                total += amount
+                        
+                    except ValueError:
+                        print("\nInvalid input: Please enter a number only.\n")
+
+                    token_selection = token_options_dict[token_menu.show()]
+                    
+            # Calculate tokens returned based on desired amount and dollar amount.
+            print(f"\n{self.name} purchased ¢{total} tokens valued at ${value}.")
         
         elif opt == 'W':
             pass
@@ -69,7 +116,7 @@ class Account:
                     self.deposit(deposit_selection)
                 
             elif selection == "Exchange":
-                exchange_options = ["[b] Buy Tokens", "[s] Sell Tokens", "[r] Return"]
+                exchange_options = ["[p] Buy Tokens", "[s] Sell Tokens", "[b] Back"]
                 exchange_options_dict = {0: 'Buy', 1: 'Sell', 2: "Back"}
 
                 exchange_menu = TerminalMenu(exchange_options, title="\n- Exchange -\n")
